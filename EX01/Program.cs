a10ba7479dc227edd83b6fcaf41a34816f098481
von_Krolikoff <milkbrother@yandex.ru>
2022-06-18 14:17:53 +0300

    +EX1

---
+++ EX01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное числo - ");
+string input = Console.ReadLine();
+int num = Convert.ToInt32(input);
+
+if (num > 100 && num <1000)
+{
+    int num1 = num % 10;
+    int num2 = ((num-num1) %100)/10;  
+    Console.WriteLine (num2);
+}
+    else Console.WriteLine("Вы ввели неправильное значение числа!");
