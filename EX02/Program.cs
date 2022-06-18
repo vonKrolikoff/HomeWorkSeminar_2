7e0cfaba351f34a8fe92e3c5b201d6923f49ee93
von_Krolikoff <milkbrother@yandex.ru>
2022-06-18 15:38:44 +0300

    +EX2

---
+++ EX02/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите числo - ");
+string input = Console.ReadLine();
+int a = input.Length;
+
+if (a <= 2)
+{
+    Console.WriteLine ("Вы ввели неправильное число!");
+}    
+    else Console.WriteLine (input[2]);
